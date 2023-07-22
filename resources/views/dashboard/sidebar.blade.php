eb1b6e48b04bbd123b5229fbe207536864917745
Stephen Jacques <jacquesstephan10@gmail.com>
2023-07-22 14:45:09 +0200

    Modification du logo sur les pages de réinitialisation de mot de passe et correction du lougout sur la navbar

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -54,6 +54,7 @@
             <form method="POST" action="{{ route('logout') }}">
                 <li class="menu au-fond">
                     <a href="{{route('logout')}}" onclick="event.preventDefault(); this.closest('form').submit();" aria-expanded="false" class="{{ Request::is('logout') ? 'dropdown-toggle active' : 'dropdown-toggle' }}">
+                        @csrf
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
                             <span>Déconnexion</span>
